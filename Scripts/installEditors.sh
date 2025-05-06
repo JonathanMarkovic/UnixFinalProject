b2e1512d284049edcbece74f4d8bcd62e1a57107
Jonathan <jonathan.markovic@outlook.com>
2025-05-06 09:59:27 -0400

    Added editor information

--- Scripts/installEditors.sh
+++ Scripts/installEditors.sh
@@ -30,7 +30,21 @@ yay -S atom
 ;;
 "Information About The Text Editors")
 echo -e "\eText Editor Information:"
-echo
+
+echo -e "1. Neovim\n"
+echo -e "Neovim is a highly extensible and customizable text editor that is a fork of vim."
+
+echo -e "2. Visual Studio Code\n"
+echo -e "Visual Studio Code is a free, powerful, and lightweight source code editor from Microsoft."
+
+echo -e "3. Nano\n"
+echo -e "Nano is a veratile and user-friendly text editor that provides essential editing capabilities."
+
+echo -e "4. Sublime Text\n"
+echo -e "Sublime Text is a sophisticated test and source code editor known for its speed, versatility, and ease of use."
+
+echo -e "5. Atom\n"
+echo -e "Atom is a free, open-source, cross-platform text editor developed by Github."
 ;;
 "Back")
 #bash UnixProject.sh
