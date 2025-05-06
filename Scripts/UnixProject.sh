e8396ebf03212961632d4d5dea2a76bac9b0e3f6
IuliaApi <143552005+IuliaApi@users.noreply.github.com>
2025-05-06 08:33:17 -0400

    Updated UnixProject.sh
    I added an Arch logo! Apologies for pushing this late.

--- Scripts/UnixProject.sh
+++ Scripts/UnixProject.sh
@@ -1,30 +1,57 @@
 #!/bin/bash
 
 displayIntroScreen() {
-clear
-echo -e "\e[1;4;100mWelcome to Arch Made Easy - Your Friend That Simplifies Your Arch Linux Setup! \e[0m"
-echo -e "\e[1;4;100mMade by Matthew Pietracupa, Jonathan Markovic & Iulia Apintilioaie\e[0m"
-echo
-echo -e "\e[0mPlease press any key to proceed with the application...\e[0m"
-read -n 1 -s
+    clear
+    echo -e "\033[38;2;23;147;209m
+                   ▄
+                  ▟█▙
+                 ▟███▙
+                ▟█████▙
+               ▟███████▙
+              ▂▔▀▜██████▙
+             ▟██▅▂▝▜█████▙
+            ▟█████████████▙
+           ▟███████████████▙
+          ▟█████████████████▙
+         ▟███████████████████▙
+        ▟█████████▛▀▀▜████████▙
+       ▟████████▛      ▜███████▙
+      ▟█████████        ████████▙
+     ▟██████████        █████▆▅▄▃▂
+    ▟██████████▛        ▜█████████▙
+   ▟██████▀▀▀              ▀▀██████▙
+  ▟███▀▘                       ▝▀███▙
+ ▟▛▀                               ▀▜▙
+\033[0m"
+
+    tput cup 2 40
+    echo -e "\033[38;2;173;216;230mWelcome to Arch Made Easy - Your Friend That Simplifies Your Arch Linux Setup! \033[0m"
+    tput cup 4 40
+    echo -e "\033[38;2;173;216;230mMade by Matthew Pietracupa, Jonathan Markovic & Iulia Apintilioaie\e[0m"
+    tput cup 6 40
+    echo -e "\033[38;2;173;216;230mPlease press any key to proceed...\033[0m"
+    read -n 1 -s
+    clear
+    displayMainMenu
 }
 
 mainMenu() {
-displayIntroScreen
-displayCategories
-mainMenuOption
+    displayIntroScreen
+    displayCategories
+    mainMenuOption
 }
 
 displayCategories() {
-clear
-echo -e "\033[1;36m\033[4mPlease choose a category to install: \033[0m"
-echo -e "\033[1m1. Code Editors   \033[0m"
-echo -e "\033[1m2. Browsers    \033[0m"
-echo -e "\033[1m3. File Managers  \033[0m"
-echo -e "\033[1m4. Terminal Emulators  \033[0m"
-echo -e "\033[1m5. Desktop Environment    \033[0m"
-echo -e "\033[1m6. System Utilities    \033[0m"
-echo -e "\033[0;31m\033[1m7. Quit \033[0;31m\033[0m"
+    clear
+    tput cup 2 40
+    echo -e "\033[38;2;173;216;230mPlease choose a category to install: \033[0m"
+    echo -e "\033[38;2;173;216;230m1. Code Editors   \033[0m"
+    echo -e "\033[38;2;173;216;230m2. Browsers    \033[0m"
+    echo -e "\033[38;2;173;216;230m3. File Managers  \033[0m"
+    echo -e "\033[38;2;173;216;230m4. Terminal Emulators  \033[0m"
+    echo -e "\033[38;2;173;216;230m5. Desktop Environment    \033[0m"
+    echo -e "\033[38;2;173;216;230m6. System Utilities    \033[0m"
+    echo -e "\033[38;2;173;216;230m7. Quit \033[0m"
 }
 
 mainMenuOption() {
@@ -71,7 +98,9 @@ mainMenuOption() {
 }
 
 loadingAnimation() {
-echo -e "\033[1mLoading...\033[0m"
-sleep 1
+    echo -e "\033[1mLoading...\033[0m"
+    sleep 1
 }
+
 mainMenu
+
