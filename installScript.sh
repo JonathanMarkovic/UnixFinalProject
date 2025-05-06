3c17b4f361dd5529933fe5f79ce888cb239bedf4
JonathanMarkovic <158488673+JonathanMarkovic@users.noreply.github.com>
2025-05-06 11:09:42 -0400

    Update installScript.sh
    General Fixes

--- installScript.sh
+++ installScript.sh
@@ -12,14 +12,14 @@ mkfs.btrfs /dev/sda2
 mount /dev/sda2 /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
-mount -o subvol=@ /dev/nvme0n1p2 /mnt
+mount -o subvol=@ /dev/sda2 /mnt
 mkdir -p /mnt/home
-mount -o subvol=@home /dev/nvme0n1p2 /mnt/home
+mount -o subvol=@home /dev/sda2 /mnt/home
 mkdir -p /mnt/efi
 mount /dev/nvme0n1p1 /mnt/efi
 
 #Installing other packages
-pacstrap -K /mnt base base-devel linux linux-firmware git
+pacstrap -K /mnt base base-devel linux linux-firmware git btrfs-progs nano
 
 #Fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -30,8 +30,7 @@ arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
 hwclock --systohc
 
-pacman -S nano
-echo -e "Uncomment the line for your locale
+echo -e "Uncomment the line for your locale"
 sleep 5
 nano /etc/locale.gen
 locale-gen
@@ -39,7 +38,7 @@ echo "KEYMAP=us" > /etc/vconsole.conf
 
 #Setting hostname
 echo "Arch" > /etc/hostname
-echo -e "127.0.0.0 localhost\n::! localhost\n127.0.1.1 Arch" > /etc/hosts
+echo -e "127.0.0.0 localhost\n::1 localhost\n127.0.1.1 Arch" > /etc/hosts
 
 #root and User creation
 echo "Set your root password"
@@ -49,7 +48,7 @@ passwd
 pacman -S grub
 pacman -S efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB  
-grub-mkconfig -o /boot/grub/grub.cfgsystem
+grub-mkconfig -o /boot/grub/grub.cfg
 
 #prepping for reboot
 pacman -S networkmanager
