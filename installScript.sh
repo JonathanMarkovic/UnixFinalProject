3aad0b99cbf540983c104647d13fbe2432335e31
JonathanMarkovic <158488673+JonathanMarkovic@users.noreply.github.com>
2025-05-06 12:52:34 -0400

    Update installScript.sh
    More testing

--- installScript.sh
+++ installScript.sh
@@ -1,32 +1,63 @@
 #This must be used in EFI mode
+# Ensure script runs in EFI mode
+if [ ! -d /sys/firmware/efi ]; then
+  echo "EFI firmware not detected. Please boot in UEFI mode."
+  exit 1
+fi
+
 #Loads the keymap for the file
 loadkeys us
 
-
 timedatectl set-ntp true
+
+# Verify disk presence before partitioning
+DISK="/dev/sda"
+if [ ! -b "$DISK" ]; then
+  echo "Disk $DISK not found. Please check your disk configuration."
+  exit 1
+fi
+
+# Confirm partitioning
+echo "WARNING: This will modify partitions on $DISK."
+read -p "Proceed? (y/n): " CONFIRM
+if [[ "$CONFIRM" != "y" ]]; then
+  echo "Partitioning canceled."
+  exit 0
+fi
+
 #Will start up the disk configuration menu
 #Installation with the script is done assuming 2 partitions on the same disk
 #cfdisk
-cfdisk /dev/sda
-mkfs.fat -F 32 /dev/sda1
-mkfs.btrfs /dev/sda2
-mount /dev/sda2 /mnt
+#cfdisk /dev/sda
+cfdisk "$DISK"
+
+#mkfs.fat -F 32 /dev/sda1
+#mkfs.btrfs -F /dev/sda2
+mkfs.fat -F 32 "${DISK}1"
+mkfs.btrfs -F "${DISK}2"
+
+mount "${DISK}2" /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 umount /mnt
-mount -o subvol=@ /dev/sda2 /mnt
+
+mount -o subvol=@ "${DISK}2" /mnt
 mkdir -p /mnt/home
-mount -o subvol=@home /dev/sda2 /mnt/home
+mount -o subvol=@home "${DISK}2" /mnt/home
 mkdir -p /mnt/efi
-mount /dev/sda1 /mnt/efi
+mount "${DISK}1" /mnt/efi
 
 #Installing other packages
-pacstrap -K /mnt base base-devel linux linux-firmware git btrfs-progs nano networkmanager
+pacstrap -K /mnt base base-devel linux linux-firmware git btrfs-progs nano networkmanager grub efibootmgr
 
 #Fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
 read -p "Enter username: " NEWUSER
+if [[ -z "$NEWUSER" ]]; then
+  echo "Invalid username. Please enter a valid username."
+  exit 1
+fi
 
 arch-chroot /mnt /bin/bash <<EOF
 
@@ -62,8 +93,6 @@ echo "$NEWUSER ALL=(ALL) ALL" >> /etc/sudoers.d/$NEWUSER
 chmod 0440 /etc/sudoers.d/$NEWUSER
 
 #Setting up Grub
-pacman -S grub
-pacman -S efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB  
 grub-mkconfig -o /boot/grub/grub.cfg
 
