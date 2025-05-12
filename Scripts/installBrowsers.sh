d05c0c7331140f4cbaefa203f03be5ab546fb1e2
IuliaApi <143552005+IuliaApi@users.noreply.github.com>
2025-05-12 10:31:47 -0400

    Update installBrowsers.sh

--- Scripts/installBrowsers.sh
+++ Scripts/installBrowsers.sh
@@ -31,13 +31,13 @@ function checkInstallers() {
 }
 
 function checkNetwork() {
-    echo -e "\033[0;36mChecking internet connection...\033[0m"
-    if ping -q -c 1 -W 2 archlinux.org &>/dev/null; then
-        echo -e "\033[0;32mNetwork connection: OK\033[0m"
-    else
-        echo -e "\033[0;31mNo internet connection. Please check your network.\033[0m"
-        exit 1
-    fi
+#    echo -e "\033[0;36mChecking internet connection...\033[0m"
+#    if ping -q -c 1 -W 2 archlinux.org &>/dev/null; then
+#        echo -e "\033[0;32mNetwork connection: OK\033[0m"
+#    else
+#        echo -e "\033[0;31mNo internet connection. Please check your network.\033[0m"
+#        exit 1
+#    fi
 }
 
 function checkStorage() {
